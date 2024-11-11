9f729aa6a17dd336eb70ad951f9576f2bde8d06f
MjuggerM <118480156+MjuggerM@users.noreply.github.com>
2024-11-11 13:49:02 +0100

    Update FourInARowView.js

--- src/games/FourInARow/views/FourInARowView.js
+++ src/games/FourInARow/views/FourInARowView.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './FourInARowView.css';
-import SettingsPopup from './Buttons/SettingsPopup';
-function FourInARowView({ gameState, makeMove, startNewGame, resetGame, undo, setTimeLimit, goToMainMenu, showSettings, toggleSettings }) {
+
+function FourInARowView({ gameState, remainingTime, makeMove, startNewGame, resetGame, undo, setTimeLimit }) {
+
+console.log("GameState: ", gameState);
+console.log("Current Player: ", gameState.currentPlayer);
+console.log("Winner: ", gameState.winner);
+console.log("Time:", gameState.timeLimit);
 
     return (
-        <div style={{ position: 'relative' }}>
+        <div>
             <div className="FourInARowView">
+
                 <div className="four-in-a-row-board">
                     {gameState.board.map((row, rowIndex) => (
                         <div key={rowIndex} className="board-row">
@@ -21,27 +27,41 @@ function FourInARowView({ gameState, makeMove, startNewGame, resetGame, undo, se
                         </div>
                     ))}
                 </div>
+                
             </div>
 
-            <button onClick={goToMainMenu} className="menu-icon"></button>
-
-            <button onClick={toggleSettings} className="settings-button">Nastavení</button>
-            {showSettings && (
-                <SettingsPopup onClose={toggleSettings} />
-            )}
-
             <button onClick={resetGame}>Reset</button>
-            <button onClick={setTimeLimit}>Nastavit čas</button>
             <button onClick={undo}>Undo</button>
+            <button onClick={setTimeLimit}>Nastavit čas</button> {/* Tlačítko pro nastavení času */}
 
             <div className="game-info">
                 <p>{gameState.currentPlayer && `Hráč ${gameState.currentPlayer} je na tahu`}</p>
                 <p>Zbývající čas na tah: {gameState.timeLimit !== null ? `${gameState.timeLimit} sekund` : 'Není nastaven'}</p>
             </div>
-
+            
             {gameState.winner && <button onClick={startNewGame}>Nová hra</button>}
+            
         </div>
     );
 }
-
+ 
 export default FourInARowView;
+
+// const numbers = [1, 2, 3, 4, 5];
+// const listItems = numbers.map((number, index) => (
+//   <li key={index}>{number}</li> // Používáme 'index' pro klíč a 'number' pro hodnotu
+// ));
+// priradi se index kazdemu pvku z pole numbers
+// number je nazev aktualne prochazeneho prvku pole,je to hodnota na aktualni pozici v poli a  používáme ji pro práci v každé iteraci
+// a používáme ji pro práci v každé iteraci
+// index je pozice aktuálního prvku v poli je nazev ktery slouzi k prideleni ID a praci s timto konkretnim prvekme pole mimo tuto iteraci
+// key={rowIndex} je dynamický atribut, který React používá pro optimalizaci renderování seznamu. upravuje pouze DOM
+
+//className={board-cell ${cell || ''}}
+// k board-cell prida 
+// if(cell !=null)
+// {pripoji k boarda-cell}
+// else(){
+// priradi prazdnou
+// }
+
