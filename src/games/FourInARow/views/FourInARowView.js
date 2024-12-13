b4f1007bf276a89b2450985e5206a8c40bb8b1cf
Bureš Martin (241347) <xbures38@vutbr.cz>
2024-12-13 21:54:45 +0100

    rozlozeni tlacitek

--- src/games/FourInARow/views/FourInARowView.js
+++ src/games/FourInARow/views/FourInARowView.js
@@ -18,6 +18,15 @@ console.log("VIEW FourInARowView:", { gameState, setTimeLimit }); //toto je tam 
 
     return (
         <div className="site">
+                {/* home button */}
+                <button className="button goToMainMenu" onClick={goToMainMenu}></button>
+
+                {/* Kontejner pro tlačítka pro časovač, nastavení */}
+                <div className="buttons-container-top">
+                        <button className="button timerButton" onClick={setTimeLimit}></button>
+                        <button className="button settings" onClick={toggleSettings}></button>
+                </div>
+
                     {/* hraci pole */}
                 <div className="Board">
                     {gameState.board.map((row, rowIndex) => (
@@ -33,29 +42,32 @@ console.log("VIEW FourInARowView:", { gameState, setTimeLimit }); //toto je tam 
                             ))}
                         </div>
                     ))}
+
+
+                         {/* Kontejner pro tlačítka desky */}
+                        <div className="buttons-container-board">
+                            <button className="button resetButton" onClick={resetGame}></button>
+                            <button className="button undoButton" onClick={undo}></button>
+
+                        </div>
                 </div>
 
-                {/* Kontejner pro tlačítka */}
-                <div className="buttons-container">
-                    <button className="button resetButton" onClick={resetGame}></button>
-                    <button className="button undoButton" onClick={undo}></button>
-                    <button className="button timerButton" onClick={setTimeLimit}></button>
-                    <button className="button settings" onClick={toggleSettings}></button>
-                    <button className="button goToMainMenu" onClick={goToMainMenu}></button>
+
+                {/* Informace o hře */}
+                <div className="gameInfo">
+                    <p>{`${gameState.message}`}</p>
+                    <p>Zbývající čas na tah: {gameState.timeLimit !== null ? `${gameState.timeLimit} sekund` : 'Není nastaven'}</p>
                 </div>
-            <div className="gameInfo">
-                <p>{`${gameState.message}`}</p>
-                <p>Zbývající čas na tah: {gameState.timeLimit !== null ? `${gameState.timeLimit} sekund` : 'Není nastaven'}</p>
-            </div>
-            
-            {showNewGameButton && (
-            <button className="button1" onClick={() => {
-                        resetGame();
-                        console.log("Tlačítko 'Nová hra' bylo kliknuto!");
-                    }}
-                >Nová hra
-            </button>
-            )}
+
+                 {/* Tlačítko pro novou hru */}
+                {showNewGameButton && (
+                <button className="button1" onClick={() => {
+                            resetGame();
+                            console.log("Tlačítko 'Nová hra' bylo kliknuto!");
+                        }}
+                    >Nová hra
+                </button>
+                )}
 
         </div>
     );
